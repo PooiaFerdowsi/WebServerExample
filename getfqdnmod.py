0786f17b295570a8841644d8b6cfbf69d64a927b
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-15 15:48:53 +0330

    Add files via upload

---
+++ getfqdnmod.py
@@ -0,0 +1,26 @@
+from _socket import gethostname,gethostbyaddr
+
+def getfqdn(name=''):
+    """Get fully qualified domain name from name.
+
+    An empty argument is interpreted as meaning the local host.
+
+    First the hostname returned by gethostbyaddr() is checked, then
+    possibly existing aliases. In case no FQDN is available, hostname
+    from gethostname() is returned.
+    """
+    name = name.strip()
+    if not name or name == '0.0.0.0':
+        name = gethostname()
+    try:
+        hostname, aliases, ipaddrs = gethostbyaddr(name)
+    except error:
+        pass
+    else:
+        aliases.insert(0, hostname)
+        for name in aliases:
+            if '.' in name:
+                break
+        else:
+            name = hostname
+    return name
